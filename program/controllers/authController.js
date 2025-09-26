525d07688388240b4727e762f978a658a7dbb5a6
ilhamfsd <ilhamfadhil1408@gmail.com>
2025-09-26 15:52:05 +0700

    add model accessToken

--- program/controllers/authController.js
+++ program/controllers/authController.js
@@ -2,32 +2,55 @@
 import User from "../models/user.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { Op } from "sequelize";
+import Token from "../models/accessToken.js";
 
 // POST /login
 export async function loginUser(req, res) {
     const { username, password } = req.body;
-    const errors = [];
+    
     if (!username || !password) {
-        errors.push({ msg: "Please fill in all fields" });
-    }
-    if (errors.length > 0) {
-        return res.status(400).json({ errors });
+        return res.status(400).json({ error: "Please fill in all fields" });
     }
     try {
+        //check user exists
         const user = await User.findOne({ where: { username } });
         if (!user) {
-            return res.status(400).json({ error: "Username doesn't exist" });
+            return res.status(400).json({ error: "Invalid username or password" });
         }
+        //check password
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
-            return res.status(401).json({ error: "Invalid credentials" });
+            return res.status(401).json({ error: "Invalid username or password" });
         }   
+
+        //generate JWT
+        const expiresAt = new Date(Date.now() +  24 * 60 * 60 * 1000); //exp 1 day
+
         const token = jwt.sign(
             { userId: user.UserId, username: user.username, role: user.role },
             process.env.JWT_SECRET,
+            { expiresIn: "1d" }
         );
-        res.json({ token, user: { id: user.UserId, username: user.username, email: user.email, role: user.role } });
+
+        //save to db
+        await Token.create({
+            userId: user.UserId,
+            token,
+            expiresAt,
+            revoked: false,
+        });
+
+        //respond in json
+        res.json({
+            message: "Login successful",
+            token,
+            user: {
+                id: user.UserId,
+                username: user.username,
+                email: user.email,
+                role: user.role,
+            }
+        });
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: "Server error" });
---
+++ program/models/accessToken.js
@@ -0,0 +1,39 @@
+//accessToken.js
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
+import User from "./user.js";
+
+const AccessToken = sequelize.define("AccessToken", {
+    tokenId: {
+        type: DataTypes.BIGINT,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    userId: {
+        type: DataTypes.BIGINT,
+        allowNull: false
+    },
+    token: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    expiresAt: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    revoked: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    }, 
+}, {
+    sequelize,
+    modelName: "AccessToken",
+    tableName: "access_token",
+    timestamps: false,
+});
+
+//relationship
+AccessToken.belongsTo(User, { foreignKey: "userId", as: "user" });
+User.hasMany(AccessToken, { foreignKey: "userId", as: "tokens" });
+
+export default AccessToken;
